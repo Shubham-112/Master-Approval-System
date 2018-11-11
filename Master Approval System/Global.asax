8cba77e94865638e1384285e1c9efbceca60f3ee
Shubham <shubham.dwc@gmail.com>
2018-11-12 01:38:05 +0530

    Initial System

---
+++ Master Approval System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Master_Approval_System.MvcApplication" Language="C#" %>
---
+++ Master Approval System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Master_Approval_System.Startup))]
+namespace Master_Approval_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
