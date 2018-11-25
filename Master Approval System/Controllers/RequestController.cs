474fadf211f0757dc46c0b73ffe313002d25df3c
Samantak <samantakmitra98@gmail.com>
2018-11-25 21:08:14 +0530

    added profile update request feature

---
+++ Master Approval System/Controllers/RequestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Master_Approval_System.Models;
+using Master_Approval_System.ViewModels;
+
+namespace Master_Approval_System.Controllers
+{
+    public class RequestController : Controller
+    {
+        // GET: Request
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult createProfileUpdateRequest()
+        {
+            ProfileUpdateRequestViewModel profile = new ProfileUpdateRequestViewModel();
+            return View(profile);
+        }
+    }
+}
---
+++ Master Approval System/ViewModels/ProfileUpdateRequestViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Master_Approval_System.ViewModels
+{
+    public class ProfileUpdateRequestViewModel
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DOB { get; set; }
+        public DateTime? JoiningDate { get; set; }
+        public int JoiningAge { get; set; }
+        public string Gender { get; set; }
+        public int RetirementAge { get; set; }
+        public string IndustryType { get; set; }
+        public string BloodGroup { get; set; }
+        public string Client { get; set; }
+        public string ClientSub { get; set; }
+        public string AgreementType { get; set; }
+        public string JobLocation { get; set; }
+        public string Division { get; set; }
+        public Boolean MaritalStatus { get; set; }
+        public string ContractType { get; set; }
+        public string PTState { get; set; }
+        public string AssociationType { get; set; }
+    }
+}
